c2814caacaf35e23333281bd804477145471b05e
Baron C <cannonbaron@gmail.com>
2021-05-23 20:01:40 -0500

    fixing bug issue with the .form-control function

--- assets/script.js
+++ assets/script.js
@@ -18,11 +18,11 @@ var userInput;
 var hourSpan;
 
 
-var interval = setInterval(function() {
+var interval = setInterval(function () {
 
   var momentNow = moment();
-  
-  $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' ' + momentNow.format('dddd').substring(0,3).toUpperCase());
+
+  $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' ' + momentNow.format('dddd').substring(0, 3).toUpperCase());
 
   $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
 
@@ -37,61 +37,61 @@ function initPage() {
 
   var time10 = JSON.parse(localStorage.getItem("10:00 am"))
   tenAm.val(time10);
-  
+
   var time11 = JSON.parse(localStorage.getItem("11:00 am"))
   elevenAm.val(time11);
-  
+
   var time12 = JSON.parse(localStorage.getItem("12:00 pm"))
   twelvePm.val(time12);
-  
+
   var time1 = JSON.parse(localStorage.getItem("01:00 pm"))
   onePm.val(time1);
-  
+
   var time2 = JSON.parse(localStorage.getItem("02:00 pm"))
   twoPm.val(time2);
-  
+
   var time3 = JSON.parse(localStorage.getItem("03:00 pm"))
   threePm.val(time3);
- 
+
   var time4 = JSON.parse(localStorage.getItem("04:00 pm"))
   fourPm.val(time4);
-  
+
   var time5 = JSON.parse(localStorage.getItem("05:00 pm"))
   fivePm.val(time5);
-  
 
-} 
 
-function background () {
-      
+}
+
+function background() {
+
   $(".form-control").each(function () {
-      var timeTest = parseInt($(this).attr("id"));
-      hour = parseInt(hour);
-      console.log(timeTest);
-      console.log(hour);
+    var timeTest = parseInt($(this).attr("id"));
+    hour = parseInt(hour);
+    console.log(timeTest);
+    console.log(hour);
 
-      if (hour > timeTest) {
-          $(this).addClass("past");
+    if (hour > timeTest) {
+      $(this).addClass("past");
 
-      } else if (hour < timeTest) {
-          $(this).addClass("future");
+    } else if (hour < timeTest) {
+      $(this).addClass("future");
 
-      } else {
-          $(this).addClass("present");
+    } else {
+      $(this).addClass("present");
 
-      }
+    }
 
   });
 
 }
 
 //Local Storage Controls(saveBtn)
-$(document).ready(function(){
+$(document).ready(function () {
   initPage()
   background()
 
- 
-  $(".saveBtn").on("click", function(){
+
+  $(".saveBtn").on("click", function () {
     userInput = $(this).siblings(".form-control").val().trim();
     console.log(userInput);
     hourSpan = $(this).siblings(".input-group-prepend").text().trim();
