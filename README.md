1659e2b909ee3ade4b9753e7fb274efd097f521d
Baron C <cannonbaron@gmail.com>
2021-05-23 19:36:46 -0500

    Adding edits to README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Work-Day-Scheduler
 A daily scheduler
+https://github.com/BCannon88/Work-Day-Scheduler
+
+
+
+![Screenshot (18)](https://user-images.githubusercontent.com/81123612/119282104-fc5ceb00-bbfd-11eb-874c-7f97ad0ef730.png)
+
+On this project, we were assigned to make a Work day scheduler. To ensure that each task remains in its time-block, we have made use of Local Storage. We have added moment.js to manage our time and have an accurate clock with date. With the help of bootstrap, we have added rows, form-control and input-group. We received our save image from font awesome.
--- assets/script.js
+++ assets/script.js
@@ -32,32 +32,32 @@ function initPage() {
 
   console.log("Current Hour " + hour);
 
-  var init9 = JSON.parse(localStorage.getItem("09:00 am"));
-  nineAm.val(init9);
+  var time9 = JSON.parse(localStorage.getItem("09:00 am"));
+  nineAm.val(time9);
 
-  var init10 = JSON.parse(localStorage.getItem("10:00 am"))
-  tenAm.val(init10);
+  var time10 = JSON.parse(localStorage.getItem("10:00 am"))
+  tenAm.val(time10);
   
-  var init11 = JSON.parse(localStorage.getItem("11:00 am"))
-  elevenAm.val(init11);
+  var time11 = JSON.parse(localStorage.getItem("11:00 am"))
+  elevenAm.val(time11);
   
-  var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
-  twelvePm.val(init12);
+  var time12 = JSON.parse(localStorage.getItem("12:00 pm"))
+  twelvePm.val(time12);
   
-  var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
-  onePm.val(init1);
+  var time1 = JSON.parse(localStorage.getItem("01:00 pm"))
+  onePm.val(time1);
   
-  var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
-  twoPm.val(init2);
+  var time2 = JSON.parse(localStorage.getItem("02:00 pm"))
+  twoPm.val(time2);
   
-  var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
-  threePm.val(init3);
+  var time3 = JSON.parse(localStorage.getItem("03:00 pm"))
+  threePm.val(time3);
  
-  var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
-  fourPm.val(init4);
+  var time4 = JSON.parse(localStorage.getItem("04:00 pm"))
+  fourPm.val(time4);
   
-  var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
-  fivePm.val(init5);
+  var time5 = JSON.parse(localStorage.getItem("05:00 pm"))
+  fivePm.val(time5);
   
 
 } 
